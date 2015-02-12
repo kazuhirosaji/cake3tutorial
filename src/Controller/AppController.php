a8c4d3289c81ca6fed0976903b16637e1614cc37
kazuhirosaji <kazuhirosaji@gmail.com>
2015-02-13 00:06:57 +0900

    Adding Login

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -37,5 +37,22 @@ class AppController extends Controller
     public function initialize()
     {
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'email',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ]
+        ]);
+        // Allow the display action so our pages controller
+        // continues to work.
+        $this->Auth->allow(['display']);
     }
 }
