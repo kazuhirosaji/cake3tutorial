6fa96dc07b914913583f2f0569d763e68a7e3586
kazuhirosaji <kazuhirosaji@gmail.com>
2015-02-14 01:10:23 +0900

    Enabling Registractions

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -10,6 +10,9 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
+    public function beforeFilter(\Cake\Event\Event $event) {
+        $this->Auth->allow(['add']);
+    }
 
     /**
      * Index method
