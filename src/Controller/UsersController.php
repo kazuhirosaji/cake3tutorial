2a014365c89e85d6ace5779511ef486046583533
kazuhirosaji <kazuhirosaji@gmail.com>
2015-02-14 11:02:32 +0900

    Restricting User Access

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -123,4 +123,26 @@ class UsersController extends AppController
         $this->Flash->success('You are now logged out.');
         return $this->redirect($this->Auth->logout());
     }
+
+    public function isAuthorized($user)
+    {
+        $action = $this->request->params['action'];
+
+        // The add and index actions are always allowed.
+        if (in_array($action, ['index', 'add', 'login', 'logout'])) {
+            return true;
+        }
+        // All other actions require an id.
+        if (empty($this->request->params['pass'][0])) {
+            return false;
+        }
+
+        // Check that the bookmark belongs to the current user.
+        $id = $this->request->params['pass'][0];
+        $current_user = $this->Users->get($id);
+        if ($current_user->id == $user['id']) {
+            return true;
+        }
+        return parent::isAuthorized($user);
+    }
 }
